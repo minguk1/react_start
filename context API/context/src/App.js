aec72d67bfcb90a14a21972d010b0971bf70b425
김민국 <eoals6511@naver.com>
2023-07-21 17:51:18 +0900

    20230721 contextapi start

---
+++ context API/context/src/App.js
@@ -0,0 +1,48 @@
+// import logo from './logo.svg';
+import './App.css';
+import {createContext, useContext} from 'react';
+
+const themeDefault = {border : '10px solid red'}
+const themeContext = createContext(themeDefault)
+
+function App() {
+  const theme = useContext(themeContext)
+  console.log('theme', theme)
+  return (
+    <div className="App root" style={theme}>
+      <h1>Hello World!</h1>
+      <Sub1 />
+    </div>
+  );
+}
+function Sub1() {
+  const theme = useContext(themeContext)
+  return (
+    <themeContext.Provider value={{border : '10px solid green'}}>
+    <div style={theme}>
+      
+      <h1>Sub1</h1>
+      <Sub2 />
+    </div>
+    </themeContext.Provider>
+  )
+}
+function Sub2() {
+  const theme = useContext(themeContext)
+  return (
+    <div style={theme}>
+      <h1>Sub2</h1>
+      <Sub3 />
+    </div>
+  )
+}
+function Sub3() {
+  const theme = useContext(themeContext)
+  return (
+    <div style={theme}>
+      <h1>Sub3</h1>
+    </div>
+  )
+}
+
+export default App;
