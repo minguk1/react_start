f8fc3452621ec59c3ff7f0f175df134197a3aa63
master <eoals6511@naver.com>
2023-07-24 00:15:31 +0900

    20230724 redux study2

--- reactRedux/react-redux/src/App.js
+++ reactRedux/react-redux/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { createStore} from 'redux';
+import { Provider, useSelector, useDispatch} from 'react-redux';
+
+function Left(props) {
+  function f(state) {
+    console.log(state)
+    return state.number
+  }
+  const number = useSelector(f)
+  
+  console.log(number)
+  return (
+    <div>
+      <h1>Left</h1>
+      <h3>number : {number}</h3>
+    </div>
+  )
+}
+function Right(props) {
+  const dispatch = useDispatch()
+  return (
+    <div>
+      <h1>Right</h1>
+      <input type="button" value="+" onClick={() => {
+          dispatch({type : 'plus'})
+      }}></input>
+    </div>
+  )
+}
+
+
+
+function reducer(currentState, action) {
+  if (currentState === undefined) {
+    return {
+      number : 1
+    }
+  }
+  const newState = {...currentState}
+  if (action.type === 'plus') {
+    newState.number++
+  }
+  return newState
+}
+const store = createStore(reducer)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Root</h1>
+      <Provider store={store}>
+        <Left />
+        <Right />
+        
+      </Provider>
     </div>
   );
 }
